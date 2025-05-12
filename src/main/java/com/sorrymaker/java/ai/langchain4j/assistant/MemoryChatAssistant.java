7805cb8b99a8793dc528522034a6c0121ac2ffb2
sorrymaker2111 <sorrymaker2111@gmail.com>
2025-05-12 22:10:01 +0800

    小智医疗完整代码

---
+++ src/main/java/com/sorrymaker/java/ai/langchain4j/assistant/MemoryChatAssistant.java
@@ -0,0 +1,16 @@
+package com.sorrymaker.java.ai.langchain4j.assistant;
+
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
+import dev.langchain4j.service.spring.AiService;
+
+import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
+
+@AiService(wiringMode = EXPLICIT,
+        chatModel = "qwenChatModel",
+        chatMemory = "chatMemory")
+public interface MemoryChatAssistant {
+
+    @UserMessage("你是我的好朋友，请用上海话回答问题，并且添加一些表情符号。 {{message}}") //{{it}}表示这里唯一的参数的占位符
+    String chat(@V("message") String message);
+}
